d0033f0876ba73398ebd872b79035769f3eea990
東條晃多 <tjkota1090@icloud.com>
2018-08-15 18:58:58 +0900

    modify current_user.present?

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,5 @@
 class FavoritesController < ApplicationController
+  before_action :authenticated_user, only: {:create, :destroy}
   def create
     favorite = current_user.favorites.create(tweet_id: params[:tweet_id])
     redirect_to tweets_path, success: "#{favorite.tweet.user.name}さんのツイートをお気に入り登録しました。"
@@ -7,5 +8,12 @@ class FavoritesController < ApplicationController
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
     redirect_to tweets_path, success: "#{favorite.tweet.user.name}さんのツイートをお気に入り解除しました。"
+  end
+
+  private
+  def authenticated_user
+    unless current_user.present?
+      redirect_to new_session_path, warning: "ログインしてください"
+    end
   end
 end
