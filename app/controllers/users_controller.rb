edd1ef2da89f6136c136b6ca0a61aaeda9321c7a
東條晃多 <tjkota1090@icloud.com>
2018-08-21 16:19:51 +0900

    modify user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
   def new
     @user = User.new
   end
@@ -13,13 +14,32 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if current_user.id == @user.id
+      if
+        @user.update(user_params)
+        redirect_to user_path(@user.id)
+      else
+        render "edit"
+      end
+    else
+      redirect_to root_path
+    end
+  end
+
   def show
-    @user = User.find(params[:id])
     @tweets = @user.tweets.all
   end
 
   private
   def user_params
     params.require(:user).permit(:name, :email, :image, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
